fa1b36d9e87c52c087257920ea6e13b0e5f32b4d
Bidexdablitz <bamideledamilola3@gmail.com>
2023-05-23 23:47:57 +0100

    Installed gh-pages and changed router to HashRouter

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import dynamicPage from "utilities/dynamicPage";
 import Layout from "components/layout/Layout";
 import googleWrapper from "utilities/googleWrapper";
@@ -76,7 +76,7 @@ export const routes = {
     },
 };
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: routes.index,
         element: <Layout />,
